27e211c188154b6c3c4bac09d6f9c4e99c3a90ac
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-08-20 14:52:39 -0300

    feat: homepage finished

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,56 +2,94 @@ import { Card } from "@/components/card";
 import billsToPay from "@/assets/icons/contasapagar.svg";
 import billsToReceive from "@/assets/icons/contasareceber.svg";
 import PayCheck from "@/assets/icons/folhasdepagamento.svg";
-
-const birthdayDayUsers = [
-  {
-    id: 1,
-    name: "Lucas Oliveira",
-    age: 31,
-    img: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
-  },
-  {
-    id: 2,
-    name: "Caio Silva",
-    age: 36,
-    img: "https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
-  },
-  {
-    id: 3,
-    name: "Caetano Borges",
-    age: 40,
-    img: "https://images.unsplash.com/photo-1590086782957-93c06ef21604?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
-  },
-];
-const birthdayMonthUsers = [
-  {
-    id: 1,
-    name: "Valéria Maria",
-    birthday: "27/07/2023",
-    img: "https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
-  },
-  {
-    id: 2,
-    name: "Caio Silva",
-    birthday: "27/07/2023",
-    img: "https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
-  },
-  {
-    id: 3,
-    name: "Bruna Oliveira de Nóbrega",
-    birthday: "27/07/2023",
-    img: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
-  },
-];
+import {
+  birthdayDayUsers,
+  birthdayMonthUsers,
+  deliveryAccounts,
+  dueAccounts,
+} from "@/utils/mock/home";
+import { Button } from "@/components/button";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+import { useState } from "react";
 
 export function Home() {
+  const [sortByFirstTable, setSortByFirstTable] = useState("description");
+  const [sortAscFirstTable, setSortAscFirstTable] = useState(true);
+
+  const handleSortFirstTable = (column: string) => {
+    if (sortByFirstTable === column) {
+      setSortAscFirstTable(!sortAscFirstTable);
+    } else {
+      setSortByFirstTable(column);
+      setSortAscFirstTable(true);
+    }
+  };
+
+  const sortedDueAccountsFirstTable = [...dueAccounts].sort((a, b) => {
+    const compareValueA = a[sortByFirstTable as keyof typeof a];
+    const compareValueB = b[sortByFirstTable as keyof typeof b];
+
+    if (
+      typeof compareValueA === "string" &&
+      typeof compareValueB === "string"
+    ) {
+      return sortAscFirstTable
+        ? compareValueA.localeCompare(compareValueB)
+        : compareValueB.localeCompare(compareValueA);
+    } else if (
+      typeof compareValueA === "number" &&
+      typeof compareValueB === "number"
+    ) {
+      return sortAscFirstTable
+        ? compareValueA - compareValueB
+        : compareValueB - compareValueA;
+    }
+
+    return 0;
+  });
+
+  const [sortBySecondTable, setSortBySecondTable] = useState("client");
+  const [sortAscSecondTable, setSortAscSecondTable] = useState(true);
+
+  const handleSortSecondTable = (column: string) => {
+    if (sortBySecondTable === column) {
+      setSortAscSecondTable(!sortAscSecondTable);
+    } else {
+      setSortBySecondTable(column);
+      setSortAscSecondTable(true);
+    }
+  };
+
+  const sortedDueAccountsSecondTable = [...deliveryAccounts].sort((a, b) => {
+    const compareValueA = a[sortBySecondTable as keyof typeof a];
+    const compareValueB = b[sortBySecondTable as keyof typeof b];
+
+    if (
+      typeof compareValueA === "string" &&
+      typeof compareValueB === "string"
+    ) {
+      return sortAscSecondTable
+        ? compareValueA.localeCompare(compareValueB)
+        : compareValueB.localeCompare(compareValueA);
+    } else if (
+      typeof compareValueA === "number" &&
+      typeof compareValueB === "number"
+    ) {
+      return sortAscSecondTable
+        ? compareValueA - compareValueB
+        : compareValueB - compareValueA;
+    }
+
+    return 0;
+  });
+
   return (
-    <div className="flex h-full p-2">
-      <div className="flex flex-col w-full">
-        <div className="flex flex-row justify-around gap-2">
-          <Card>
-            <div className="flex flex-row p-4">
-              <div>
+    <div className="flex h-full p-5">
+      <div className="flex flex-col w-full gap-5">
+        <div className="flex flex-row justify-between gap-2">
+          <Card className="w-80">
+            <div className="flex flex-row p-4 h-full">
+              <div className="flex flex-col justify-around">
                 <p className="text-sm font-medium mb-7 mr-4">
                   Contas à Pagar Vencidas
                 </p>
@@ -65,10 +103,9 @@ export function Home() {
               />
             </div>
           </Card>
-
-          <Card>
-            <div className="flex flex-row p-4">
-              <div>
+          <Card className="w-80">
+            <div className="flex flex-row p-4  h-full">
+            <div className="flex flex-col justify-around">
                 <p className="text-sm font-medium mb-7 mr-4">
                   Contas à Receber Pendencias
                 </p>
@@ -82,10 +119,9 @@ export function Home() {
               />
             </div>
           </Card>
-
-          <Card>
+          <Card className="w-80">
             <div className="flex flex-row p-4">
-              <div>
+            <div className="flex flex-col justify-around">
                 <p className="text-sm font-medium mb-7 mr-4">
                   Folhas de Pagamento em Aberto
                 </p>
@@ -95,9 +131,255 @@ export function Home() {
             </div>
           </Card>
         </div>
+
+        <div className="flex flex-row justify-around gap-2 h-[380px]">
+          <Card className="w-full overflow-y-auto">
+            <p className="text-sm font-medium text-left w-full p-8 items-center">
+              Contas à vencer nos próximos 5 dias
+            </p>
+
+            <div className="flex w-full">
+              <table className="border-collapse w-full ml-3 mr-3">
+                <thead className="text-[#64748B] text-sm font-light text-left">
+                  <tr>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2 w-1/6"
+                      onClick={() => handleSortFirstTable("description")}
+                    >
+                      <div className="flex flex-row gap-2 items-center">
+                        DESCRIÇÃO
+                        {sortByFirstTable === "description" ? (
+                          sortAscFirstTable ? (
+                            <BsChevronDown color="black" />
+                          ) : (
+                            <BsChevronUp color="black" />
+                          )
+                        ) : (
+                          <BsChevronDown />
+                        )}
+                      </div>
+                    </th>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2 w-1/6"
+                      onClick={() => handleSortFirstTable("expensesGroup")}
+                    >
+                      <div className="flex flex-row">
+                        <div className="flex flex-row gap-2 items-center">
+                          GRUPO DE DESPESA
+                          {sortByFirstTable === "expensesGroup" ? (
+                            sortAscFirstTable ? (
+                              <BsChevronDown color="black" />
+                            ) : (
+                              <BsChevronUp color="black" />
+                            )
+                          ) : (
+                            <BsChevronDown />
+                          )}
+                        </div>
+                      </div>
+                    </th>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2"
+                      onClick={() => handleSortFirstTable("dueDate")}
+                    >
+                      <div className="flex flex-row">
+                        <div className="flex flex-row gap-2 items-center">
+                          DATA DE VENCIMENTO
+                          {sortByFirstTable === "dueDate" ? (
+                            sortAscFirstTable ? (
+                              <BsChevronDown color="black" />
+                            ) : (
+                              <BsChevronUp color="black" />
+                            )
+                          ) : (
+                            <BsChevronDown />
+                          )}
+                        </div>
+                      </div>
+                    </th>
+                    <th className="p-2 pl-4">STATUS</th>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2"
+                      onClick={() => handleSortFirstTable("value")}
+                    >
+                      <div className="flex flex-row">
+                        <div className="flex flex-row gap-2 items-center">
+                          VALOR
+                          {sortByFirstTable === "value" ? (
+                            sortAscFirstTable ? (
+                              <BsChevronDown color="black" />
+                            ) : (
+                              <BsChevronUp color="black" />
+                            )
+                          ) : (
+                            <BsChevronDown />
+                          )}
+                        </div>
+                      </div>
+                    </th>
+                    <th className="p-2 pl-4"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sortedDueAccountsFirstTable.map((conta) => (
+                    <tr
+                      key={conta.id}
+                      className="text-left text-[#191D23] font-normal text-sm"
+                    >
+                      <td className="border-b border-t p-3">
+                        {conta.description}
+                      </td>
+                      <td className="border-b border-t p-3">
+                        {conta.expensesGroup}
+                      </td>
+                      <td className="border-b border-t p-3">{conta.dueDate}</td>
+                      <td className="border-b border-t p-3">
+                        <Button className="bg-[#ECFDF5] text-[#FF0000] font-semibold pl-7 pr-7">
+                          {conta.status}
+                        </Button>
+                      </td>
+                      <td className="border-b border-t p-3">
+                        R${" "}
+                        {Number(conta.value).toLocaleString("pt-BR", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
+                      </td>
+                      <td className="border-b border-t pt-2 pb-2 text-center">
+                        <Button className="bg-green-800 hover:bg-green-600 text-[white] pl-7 pr-7">
+                          Pagar
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </Card>
+        </div>
+
+        <div className="flex flex-row justify-around gap-2 h-[380px] pb-4">
+          <Card className="w-full overflow-y-auto">
+            <p className="text-sm font-medium text-left w-full p-8 items-center">
+              Recebimentos dos próximos 5 dias
+            </p>
+
+            <div className="flex w-full">
+              <table className="border-collapse w-full ml-3 mr-3">
+                <thead className="text-[#64748B] text-sm font-light text-left">
+                  <tr>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2 w-1/6"
+                      onClick={() => handleSortSecondTable("client")}
+                    >
+                      <div className="flex flex-row gap-2 items-center">
+                      CLIENTE
+                        {sortBySecondTable === "client" ? (
+                          sortAscSecondTable ? (
+                            <BsChevronDown color="black" />
+                          ) : (
+                            <BsChevronUp color="black" />
+                          )
+                        ) : (
+                          <BsChevronDown />
+                        )}
+                      </div>
+                    </th>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2 w-1/6"
+                      onClick={() => handleSortSecondTable("construction")}
+                    >
+                      <div className="flex flex-row gap-2 items-center">
+                      OBRA
+                        {sortBySecondTable === "construction" ? (
+                          sortAscSecondTable ? (
+                            <BsChevronDown color="black" />
+                          ) : (
+                            <BsChevronUp color="black" />
+                          )
+                        ) : (
+                          <BsChevronDown />
+                        )}
+                      </div>
+                    </th>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2"
+                      onClick={() => handleSortSecondTable("deliveryDate")}
+                    >
+                      <div className="flex flex-row gap-2 items-center">
+                      DATA DO RECEBIMENTO
+                        {sortBySecondTable === "deliveryDate" ? (
+                          sortAscSecondTable ? (
+                            <BsChevronDown color="black" />
+                          ) : (
+                            <BsChevronUp color="black" />
+                          )
+                        ) : (
+                          <BsChevronDown />
+                        )}
+                      </div>
+                    </th>
+                    <th className="p-2 pl-4">STATUS</th>
+                    <th
+                      className="p-2 pl-4 cursor-pointer items-center gap-2"
+                      onClick={() => handleSortSecondTable("value")}
+                    >
+                      <div className="flex flex-row gap-2 items-center">
+                      VALOR
+                        {sortBySecondTable === "value" ? (
+                          sortAscSecondTable ? (
+                            <BsChevronDown color="black" />
+                          ) : (
+                            <BsChevronUp color="black" />
+                          )
+                        ) : (
+                          <BsChevronDown />
+                        )}
+                      </div>
+                    </th>
+                    <th className="p-2 pl-4"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sortedDueAccountsSecondTable.map((conta) => (
+                    <tr
+                      key={conta.id}
+                      className="text-left text-[#191D23] font-normal text-sm"
+                    >
+                      <td className="border-b border-t p-3">{conta.client}</td>
+                      <td className="border-b border-t p-3">
+                        {conta.construction}
+                      </td>
+                      <td className="border-b border-t p-3">
+                        {conta.deliveryDate}
+                      </td>
+                      <td className="border-b border-t p-3">
+                        <Button className="bg-[#ECFDF5] text-[#064E3B] font-semibold pl-7 pr-7">
+                          {conta.status}
+                        </Button>
+                      </td>
+                      <td className="border-b border-t p-3">
+                        R${" "}
+                        {Number(conta.value).toLocaleString("pt-BR", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
+                      </td>
+                      <td className="border-b border-t pt-2 pb-2 text-center">
+                        <Button className="bg-blue-800 hover:bg-blue-600 text-white pl-7 pr-7">
+                          Recebido
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </Card>
+        </div>
       </div>
 
-      <div className="flex flex-col gap-3 pl-2">
+      <div className="flex flex-col gap-3 pl-5">
         <div>
           <Card className="w-72 h-80 flex p-4">
             <p className="mb-5 border-b border-black p-2 pb-3 font-bold text-base w-full text-center">
@@ -115,7 +397,9 @@ export function Home() {
                   </div>
                   <div>
                     <p className="font-semibold text-sm mb-1">{user.name}</p>
-                    <p className="font-light text-xs text-[#343439] ">{user.age} anos</p>
+                    <p className="font-light text-xs text-[#343439] ">
+                      {user.age} anos
+                    </p>
                   </div>
                 </div>
               ))}
@@ -129,19 +413,21 @@ export function Home() {
             </p>
             <div className="flex flex-col justify-start items-start w-full pl-3 overflow-y-auto">
               {birthdayMonthUsers.map((user) => (
-                 <div className="flex flex-row gap-6 mb-2 mt-2">
-                 <div>
-                   <img
-                     src={user.img}
-                     alt="Foto do Usuario"
-                     className="rounded-full h-10 w-10 object-cover"
-                   />
-                 </div>
-                 <div>
-                   <p className="font-semibold text-sm mb-1">{user.name}</p>
-                   <p className="font-light text-xs text-[#343439] ">{user.birthday}</p>
-                 </div>
-               </div>
+                <div className="flex flex-row gap-6 mb-2 mt-2">
+                  <div>
+                    <img
+                      src={user.img}
+                      alt="Foto do Usuario"
+                      className="rounded-full h-10 w-10 object-cover"
+                    />
+                  </div>
+                  <div>
+                    <p className="font-semibold text-sm mb-1">{user.name}</p>
+                    <p className="font-light text-xs text-[#343439] ">
+                      {user.birthday}
+                    </p>
+                  </div>
+                </div>
               ))}
             </div>
           </Card>
